1a09061753e8bf36f61acc99af59713b6d3236ad
Eric Hiller <eric@hiller.pro>
2018-04-12 11:28:36 -0700

    Updates to Serial in/out, sampling

--- temp-controller-esp32.ino
+++ temp-controller-esp32.ino
@@ -3,33 +3,51 @@
 #include <U8g2lib.h>
 
 
-#include "driver/mcpwm.h"
-#include "soc/mcpwm_reg.h"
-#include "soc/mcpwm_struct.h"
+#include "driver/ledc.h"
+#include "soc/ledc_reg.h"
+#include "soc/ledc_struct.h"
+
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
 #include "secrets.h"
 
+#define DUTY_RESOLUTION LEDC_TIMER_11_BIT    // 11 bits gives me 0-2047 levels of resolution
+#define PWM_FREQUENCY 25000
+#define commandBuf_MAX 64
+
+
 U8G2_SH1106_128X64_NONAME_1_4W_HW_SPI u8g2( U8G2_R0, /* cs=*/5, /* dc=*/21, /* reset=*/22 );
-// U8G2_SH1106_128X64_NONAME_2_4W_HW_SPI u8g2( U8G2_R0, /* cs=*/5, /* dc=*/21, /* reset=*/22 );
-// U8G2_SH1106_128X64_VCOMH0_1_4W_HW_SPI
-// U8G2_SH1106_128X64_VCOMH0_2_4W_HW_SPI
-// U8G2_SH1106_128X64_WINSTAR_1_4W_HW_SPI u8g2( U8G2_R0, /* cs=*/5, /* dc=*/21, /* reset=*/22 );
-// U8G2_SH1106_128X64_WINSTAR_2_4W_HW_SPI
-// 1,2,F
 
 int   sensorPin   = 33;    // select the input pin for the Sensor :: Pin 35 is ADC1_CHANNEL_7
 int   sensorValue = 0;     // variable to store the value coming from the sensor
 float centigrade  = 0;
 float farenheit   = 0;
 
+int      PWM_PIN     = 19;
+uint32_t DUTY_MAX    = pow( 2, (double)DUTY_RESOLUTION );
+uint32_t PWM_PERCENT = 50;
+
+ledc_channel_config_t ledc_channel = {
+    .gpio_num   = PWM_PIN,
+    .speed_mode = LEDC_HIGH_SPEED_MODE,
+    .channel    = LEDC_CHANNEL_0,
+    .intr_type  = LEDC_INTR_DISABLE,
+    .timer_sel  = LEDC_TIMER_0,
+    .duty       = ( uint32_t )( ( (double)PWM_PERCENT / 100 ) * (double)DUTY_MAX )
+
+};
+
+ledc_timer_config_t ledc_timer = {};
+
 #define BAUD_RATE 115200
 
-#define WIFI_TIMEOUT 15000 // 15s timeout
+#define WIFI_TIMEOUT 15000    // 15s timeout
 WiFiServer server( 80 );
 
 
 /*** GLOBALS ***/
-String inputString = "";    // a String to hold incoming data
+char commandBuf[commandBuf_MAX]     = "";    // a String to hold incoming data
 
 /** Menu
  */
@@ -40,40 +58,36 @@ uint8_t MENU_pin_up     = 4;
 uint8_t MENU_pin_down   = 12;
 uint8_t MENU_pin_home   = 14;
 
-
-// NOTE:  The most recent version of the encoder from github is required
-// for the ESP32: https://github.com/PaulStoffregen/Encoder
-// #include "Encoder.h"
-
-// MCPWM Pins
-static const unsigned Q = 16;    // GPIO16; THE  ** LED **
-// static const unsigned Q_BAR = 8; // GPIO17;
-
-// static mcpwm_dev_t *MCPWM[2] = {&MCPWM0, &MCPWM1};
-
 // Forward declarations
-static void init_PWM_based_on_state( void );
-static void set_frequency( uint32_t freq );
-static void set_duty_cycle( float dc );
-static void setup_mcpwm( );
+void set_duty_percent( int percent );
 
 
 
 void setup( ) {
-
+	ledc_timer.duty_resolution = LEDC_TIMER_11_BIT;       // resolution of PWM duty
+	ledc_timer.freq_hz         = PWM_FREQUENCY;           // frequency of PWM signal
+	ledc_timer.speed_mode      = LEDC_HIGH_SPEED_MODE;    // timer mode
+	ledc_timer.timer_num       = LEDC_TIMER_0;            // timer index
+	ledc_timer_config( &ledc_timer );
+	ledc_channel_config( &ledc_channel );
 
 	// Set up serial port.
 	Serial.begin( BAUD_RATE );
-	// reserve 200 bytes for the inputString:
-	inputString.reserve( 200 );
 
-	int wifi_begin = millis();
+	int wifi_begin = millis( );
 	WiFi.begin( ssid, password );
 
-	while( WiFi.status( ) != WL_CONNECTED && !WiFi.localIP( ) && millis() < wifi_begin+15000) {
+	while( WiFi.status( ) != WL_CONNECTED && !WiFi.localIP( ) && millis( ) < wifi_begin + 15000 ) {
 		delay( 500 );
 		Serial.print( "." );
 	}
+
+	Serial.println( "\n:LEDC INIT VALUES\nledc_timer.duty_resolution:\t" + String( ledc_timer.duty_resolution ) + "\n"
+	                                                                                                              "ledc_channel.freq_hz:\t" +
+	                String( ledc_timer.freq_hz ) + "\n" +
+	                "ledc_channel.duty:\t" + ledc_channel.duty + "\n"
+	                                                             "DUTY_MAX:\t" +
+	                DUTY_MAX + "\n" );
 
 	pinMode( MENU_pin_select, INPUT_PULLUP );
 	pinMode( MENU_pin_next, INPUT_PULLUP );
@@ -88,24 +102,11 @@ void setup( ) {
 	Serial.println( WiFi.localIP( ) );
 	server.begin( );
 
-	setup_mcpwm( );
-
-	init_PWM_based_on_state( );
-
 	// u8g2.begin( analogInputToDigitalPin( MENU_pin_select ), analogInputToDigitalPin( MENU_pin_next ), analogInputToDigitalPin( MENU_pin_prev ), analogInputToDigitalPin( MENU_pin_up ), MENU_pin_down, MENU_pin_home );    // https://github.com/olikraus/u8g2/wiki/u8g2reference#begin
-	u8g2.begin( MENU_pin_select , MENU_pin_next,  MENU_pin_prev ,  MENU_pin_up , MENU_pin_down, MENU_pin_home );    // https://github.com/olikraus/u8g2/wiki/u8g2reference#begin
+	u8g2.begin( MENU_pin_select, MENU_pin_next, MENU_pin_prev, MENU_pin_up, MENU_pin_down, MENU_pin_home );    // https://github.com/olikraus/u8g2/wiki/u8g2reference#begin
 
 	Serial.println( "pins set" );
 }
-
-
-enum States {
-	DUTY_CYCLE,    ///< Encoder sets duty cycle
-	FREQ,          ///< Encoder sets frequency
-	MAX_STATES
-};                                           // enum States
-static int values[MAX_STATES] = {50, 50};    // Initialized duty cycle and frequency to 50%
-
 
 char buf_temp[16];
 char buf_time[16];
@@ -113,25 +114,6 @@ char print_action[20] = "none";
 void loop( ) {
 	// Serial.println( "creating client wifi" );
 	WiFiClient client = server.available( );    // listen for incoming clients
-	uint8_t    r;
-
-	// while( true ) {
-	// 	u8g2.setFont( u8x8_font_chroma48medium8_r );
-	// 	r = u8g2.userInterfaceMessage( "Message", "Box", NULL, " Ok \n Cancel " );
-	// 	Serial.println( "userInterfaceMessage ... " );
-	// 	if( r == 0 ) {
-	// 		u8g2.userInterfaceMessage( "You pressed the", "Home/Quit", "Button", " Ok " );
-	// 		Serial.println( "was 0" );
-
-	// 	} else if( r == 1 ) {
-	// 		u8g2.userInterfaceMessage( "You selected the", "Ok", "Button", " Ok " );
-	// 		Serial.println( "was 1" );
-
-	// 	} else if( r == 2 ) {
-	// 		u8g2.userInterfaceMessage( "You selected the", "Cancel", "Button", " Ok " );
-	// 		Serial.println( "was 2" );
-	// 	}
-	// }
 
 	if( client ) {
 		Serial.println( "New Client" );
@@ -152,7 +134,7 @@ void loop( ) {
 
 						// the content of the HTTP response follows the header:
 						client.print( "<b>STATUS:</b> " );
-						client.print( values[0] );
+						client.print( ledc_channel.duty );
 						client.print( "<br /><br /><a href=\"/H\">UP</a><a href=\"/HT\">+10</a><br />>" );
 						client.print( "<a href=\"/L\">DOWN</a><a href=\"/LT\">-10</a><br>" );
 
@@ -170,22 +152,18 @@ void loop( ) {
 				// Check to see if the client request was "GET /H" or "GET /L":
 				if( currentLine.endsWith( "GET /H" ) ) {
 					// digitalWrite( 5, HIGH );    // GET /H turns the LED on
-					values[0]++;
-					init_PWM_based_on_state( );
+					set_duty_percent( PWM_PERCENT + 1 );
 				}
 				if( currentLine.endsWith( "GET /HT" ) ) {
 					// digitalWrite( 5, HIGH );    // GET /H turns the LED on
-					values[0]=values[0]+10;
-					init_PWM_based_on_state( );
+					set_duty_percent( PWM_PERCENT + 10 );
 				}
 				if( currentLine.endsWith( "GET /L" ) ) {
-					values[0]--;
-					init_PWM_based_on_state( );
+					set_duty_percent( PWM_PERCENT - 1 );
 					// digitalWrite( 5, LOW );    // GET /L turns the LED off
 				}
 				if( currentLine.endsWith( "GET /LT" ) ) {
-					values[0]=values[0]-10;
-					init_PWM_based_on_state( );
+					set_duty_percent( PWM_PERCENT - 10 );
 					// digitalWrite( 5, LOW );    // GET /L turns the LED off
 				}
 			}
@@ -197,19 +175,24 @@ void loop( ) {
 	/******************************************
 	 ****  read the value from the sensor: ****
 	 ******************************************/
+	analogRead( sensorPin );
+	delay(10);
 	sensorValue = analogRead( sensorPin );
 
-	Serial.print( millis( ) );
-	Serial.print( " ms || " );
-	Serial.print( sensorValue );
-	Serial.print( " milliVolt || " );
-	Serial.print( centigrade );
-	Serial.print( " degrees C || " );
+	// Serial.print( millis( ) );
+	// Serial.print( " ms || " );
+	// Serial.print( sensorValue );
+	// Serial.print( " milliVolt || " );
+	// Serial.print( centigrade );
+	// Serial.print( " degrees C || " );
 
 	centigrade = ( sensorValue - 500 ) / 10;
 	farenheit  = ( centigrade * 9.0 / 5.0 ) + 32.0;
-	Serial.print( farenheit );
-	Serial.println( " degrees F" );
+	// Serial.print( farenheit );
+	// Serial.println( " degrees F" );
+
+	measure_adc_multisample_raw();
+
 
 
 	// u8g2.setFont( u8g2_font_logisoso30_tn );
@@ -236,6 +219,7 @@ void loop( ) {
 	/******************************************
 	 ****       check for menu input       ****
 	 ******************************************/
+	/*
 	int8_t event = u8g2.getMenuEvent( );
 	switch( event ) {
 		case U8X8_MSG_GPIO_MENU_SELECT:
@@ -320,191 +304,89 @@ void loop( ) {
 		Serial.print( "HIGH" );
 	}
 	Serial.println( "" );
-
+**/
 
 	// print the string when a newline arrives:
-	while( Serial.available( ) ) {
-		Serial.println( "Serial data available" );
-		char inChar = (char)Serial.read( );
-		inputString += inChar;
-		if( inChar == ';' || inChar == '\n' ) {
-			Serial.println( inputString );
-			if( inputString.substring( 0, 3 ) == "GET" ) {
-			}
-			if( inputString.substring( 0, 3 ) == "SET" ) {
-				Serial.println( "SET" );
-
-				if( inputString.substring( 4, 8 ) == "duty" ) {
-					Serial.println( "duty" );
-
-					values[0] = inputString.substring( 9 ).toInt( );
-					Serial.println( values[0] );
-
-					init_PWM_based_on_state( );
-				}
-			}
 
 
-
-
+	// Process any incoming characters from the serial port
+	while( Serial.available( ) > 0 ) {
+		char c = Serial.read( );
+		// Add any characters that aren't the end of a command (semicolon) to the input buffer.
+		if( c != ';' && c != '\n' ) {
+			c = toupper( c );
+			strncat( commandBuf, &c, 1 );
+		} else {
+			// Parse the command because an end of command token was encountered.
+			parseCommand( commandBuf );
 
 			// clear the string:
-			inputString = "";
+			memset(commandBuf, 0 , commandBuf_MAX);
 		}
 	}
 }
 
+void parseCommand( char *commandBuf ) {
+	Serial.println("parsing command: " + (String)commandBuf);
+	if( strcmp(commandBuf,"SET DUTY" ) == 0 ) {
+		int new_pwm_percent = ((String)commandBuf).substring( 9 ).toInt( );
+		Serial.println( "Setting new duty cycle percentage to:" + new_pwm_percent );
+		set_duty_percent( new_pwm_percent );
+	}
+	// clear the string:
+	commandBuf = "";
+}
 
-//    static unsigned state = 0U;
-//    static int last_val_in_state = 0;
-//    static bool first_time_through_loop = true;
+int NO_OF_SAMPLES = 64;
+adc_unit_t ADC_UNIT = ADC_UNIT_1;
 
-//    if ( first_time_through_loop )
-//    {
-// 	  encoder.write(values[state]);
-// 	  first_time_through_loop = false;
-// 	  last_val_in_state = values[state];
-//    }
 
-//    values[state] = encoder.read();
+void measure_adc_multisample_raw(){
+	static esp_adc_cal_characteristics_t *adc_chars;
 
-//    if ( button_cnt > 0 )
-//    {
-// 	  // button has been pressed
-// 	  state++;
-// 	  if (state > MAX_STATES-1)
-// 		 state = 0;
+	adc_chars = (esp_adc_cal_characteristics_t*)calloc(1, sizeof(esp_adc_cal_characteristics_t));
+	esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT, ADC_ATTEN_11db, ADC_WIDTH_BIT_12, 1100, adc_chars);
+	//Check type of calibration value used to characterize ADC
+	Serial.println(val_type);
+	//Continuously sample ADC1
+	while (1) {
+		uint32_t adc_reading = 0;
+		//Multisampling
+		for (int i = 0; i < NO_OF_SAMPLES; i++) {
+			if (ADC_UNIT == ADC_UNIT_1) {
+				adc_reading += adc1_get_raw((adc1_channel_t)sensorPin);
+			} else {
+				int raw;
+				adc2_get_raw((adc2_channel_t)sensorPin, ADC_WIDTH_BIT_12, &raw);
+				adc_reading += raw;
+			}
+		}
+		adc_reading /= NO_OF_SAMPLES;
+		//Convert adc_reading to voltage in mV
+		uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+		printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 
-// 	  encoder.write(values[state]);
-// 	  last_val_in_state = values[state];
+}
 
-// 	  button_cnt = 0;
-//    }
 
-//    // React to whatever changes have been made.
-//    if ( values[state] != last_val_in_state )
-//    {
-// 	  switch(state)
-// 	  {
-// 	  case DUTY_CYCLE : values[state] = enc_set_dc(values[state]);
-// 		 break;
-// 	  case FREQ : values[state] = enc_set_freq(values[state]);
-// 		 break;
-// 	  } // switch(state)
 
-// 	  last_val_in_state = values[state];
-// 	  encoder.write(values[state]);
-//    }
-
-//    Serial.println("State: " + String(state)
-// 				  + " Value: " + String(values[state]));
-
-//    delay(500);
-
-// } // loop()
-
-static int enc_set_dc( int val ) {
-	// val is a percentage
-	// Returns val
+void set_duty_percent( int percent ) {
+	// returns duty in fractional
 	static constexpr int MAX = {100};
-	static constexpr int MIN = {10};
+	static constexpr int MIN = {20};
 
 	// Clamp the value
-	int pct = ( val >= MIN ) ? val : MIN;
-	pct     = ( pct <= MAX ) ? pct : MAX;
+	percent = ( percent >= MIN ) ? percent : MIN;
+	percent = ( percent <= MAX ) ? percent : MAX;
 
 	// Compute the controlling values.
-	set_duty_cycle( pct / 2.0 );
-	Serial.println( "Duty Cycle: " + String( pct / 2.0 ) );
+	uint32_t duty = ( uint32_t )( ( (double)percent / 100 ) * (double)DUTY_MAX );
 
-	return pct;
-}    // enc_set_dc()
+	Serial.println( "Setting Duty Cycle: " + String( percent ) + "% ; " + String( duty ) + "/" + DUTY_MAX );
 
-static int enc_set_freq( int val ) {
-	// val is a percentage.
-	// Note that setting dy will change frequency because it changes the slope
-	// and the number of steps per cycle is determined by the slope and the
-	// amplitude.
-	static constexpr int      MAX      = {100};
-	static constexpr int      MIN      = {0};
-	static constexpr uint32_t MIN_FREQ = {500};
-	static constexpr uint32_t MAX_FREQ = {10000};
-
-	// Clamp the value
-	int pct = ( val >= MIN ) ? val : MIN;
-	pct     = ( pct <= MAX ) ? pct : MAX;
-
-	// Compute the new frequency
-	int new_freq = static_cast<int>( ( pct / 100.0 ) * ( MAX_FREQ - MIN_FREQ ) ) + MIN_FREQ;
-
-	Serial.println( "Freq: " + String( new_freq ) );
-
-	set_frequency( new_freq );
-
-	return pct;
-}    // enc_set_freq()
-
-static void init_PWM_based_on_state( ) {
-	values[DUTY_CYCLE] = enc_set_dc( values[DUTY_CYCLE] );
-	values[FREQ]       = enc_set_freq( values[FREQ] );
-}    // init_PWM
-
-
-static void setup_mcpwm_pins( ) {
-	Serial.println( "initializing mcpwm control gpio...n" );
-	mcpwm_gpio_init( MCPWM_UNIT_0, MCPWM0A, Q );
-	//    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, Q_BAR);
-}    // setup_pins()
-
-
-static void setup_mcpwm( ) {
-	setup_mcpwm_pins( );
-
-	Serial.println( "Configuring initial parameters ...\n" );
-	// Not that this is initial configuration only, further, more
-	// detailed configuration will often be required.
-	mcpwm_config_t pwm_config;
-	pwm_config.frequency = 10000;    //frequency = 1000Hz
-	pwm_config.cmpr_a    = 30.0;     //duty cycle of PWMxA = 30.0%
-	// pwm_config.cmpr_b       = 30.0;                            //duty cycle of PWMxB = 30.0%
-	pwm_config.counter_mode = MCPWM_UP_DOWN_COUNTER;           // Up-down counter (triangle wave)
-	pwm_config.duty_mode    = MCPWM_DUTY_MODE_0;               // Active HIGH
-	mcpwm_init( MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config );    //Configure PWM0A & PWM0B with above settings
-
-	// The following calls set what happens on transitions relative to the
-	// comparators.  The API documentation doesn't make this clear, but
-	// I believe that these set the 'actions' described under "PWM Signal Generation"
-	// in section 15.3.3 on pg. 341 of the reference manual (v2.7).  What is odd is
-	// that 'toggle' is not supported.  I don't need these for my application, but
-	// I did experiment with them and they work.
-	// mcpwm_set_signal_low(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A);
-	// mcpwm_set_signal_high(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B);
-
-	// MCPWMXA to duty mode 1 and MCPWMXB to duty mode 0 or vice versa
-	// will generate MCPWM compliment signal of each other, there are also
-	// other ways to do it
-	mcpwm_set_duty_type( MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, MCPWM_DUTY_MODE_0 );
-	// mcpwm_set_duty_type( MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, MCPWM_DUTY_MODE_1 );
-
-	// Allows the setting of a minimum dead time between transitions to prevent
-	// "shoot through" a totem-pole switch pair, which can be disastrous.
-	//	mcpwm_deadtime_enable(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_BYPASS_FED, 100, 100);   //Deadtime of 10us
-
-	mcpwm_start( MCPWM_UNIT_0, MCPWM_TIMER_0 );
-}    // setup_mcpwm
-
-static void set_duty_cycle( float dc ) {
-	// dc is the duty cycle, as a percentage (not a fraction.)
-	// (e.g. 30.0 is 30%)
-	mcpwm_set_duty( MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, dc );
-	// mcpwm_set_duty( MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, 100.0 - dc );
-}    // set_duty_cycle()
-
-static void set_frequency( uint32_t freq ) {
-	// freq is the desired frequency, in Hz.
-	mcpwm_set_frequency( MCPWM_UNIT_0, MCPWM_TIMER_0, freq * 2U );
-}    // Set the PWM frequency.
-
-// void buttonISR( void ) {
-// button_cnt++;
-// }    // buttonISR()
+	ledc_set_duty( ledc_channel.speed_mode, ledc_channel.channel, duty );
+	ledc_update_duty( ledc_channel.speed_mode, ledc_channel.channel );
+	PWM_PERCENT = percent;
+}    // set_duty_percent()
